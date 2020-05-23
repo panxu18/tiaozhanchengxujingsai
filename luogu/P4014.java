2e7cbaa68061f49eb5c1c00cba8defb0aa830d17
panxu18 <49966697+panxu18@users.noreply.github.com>
2020-05-23 11:56:54 +0800

    最小费用流，带权最大匹配模板题

--- luogu/P4014.java
+++ luogu/P4014.java
@@ -3,11 +3,11 @@ package tiaozhanchengxujingsai.luogu;
 import java.io.*;
 import java.util.Arrays;
 
-import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class P4014 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         new P4014().solve();
     }
     PrintWriter out = new PrintWriter(System.out);
@@ -22,100 +22,127 @@ public class P4014 {
         return in.sval;
     }
 
-    int MAXN = 105;
+    int MAXN = 3000;
     int INF = 1000000007;
-    int T, N, M;
     long ans;
-    long[][] map = new long[MAXN][MAXN];
+    int N;
     private void solve() throws IOException {
         N = (int) readDouble();
-        for (int i = 0; i < MAXN; i++) {
-            Arrays.fill(map[i], INF);
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                map[i][j] = (long)-readDouble();
-            }
-        }
-        km();
-        out.println(-ans);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                map[i][j] = -map[i][j];
-            }
-        }
-        km();
-        out.println(ans);
+        source = N <<1;
+        sink = source +1;
 
+        for (int i = 0; i < N; i++) {
+            addEdge(source, i, 0);
+            addreEdge(source, i, 0);
+            for (int j = 0; j < N; j++) {
+                long cost = (long) readDouble();
+                addEdge(i, j+N, cost);
+                addreEdge(i, j+N, -cost);
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            addEdge(i+N, sink, 0);
+            addreEdge(i+N, sink, 0);
+        }
+        zkw();
+        out.println(ans);
+        head = rehead;
+        zkw();
+        out.println(-ans);
         out.flush();
 
     }
 
-    int[] match = new int[MAXN];
-    boolean[] visy = new boolean[MAXN];
-    long[] lablex = new long[MAXN];
-    long[] labley = new long[MAXN];
+    private void addreEdge(int u, int v, long cost) {
+        rehead[u] = new Edge(u, v, 1, cost, rehead[u], null);
+        rehead[v] = new Edge(v, u, 0, -cost, rehead[v], rehead[u]);
+        rehead[u].rEdge = rehead[v];
+    }
+    private void addEdge(int u, int v, long cost) {
+        head[u] = new Edge(u, v, 1, cost, head[u], null);
+        head[v] = new Edge(v, u, 0, -cost, head[v], head[u]);
+        head[u].rEdge = head[v];
+    }
+
+    int source, sink;
+    boolean[] used = new boolean[MAXN];
+    boolean[] inq = new boolean[MAXN];
+    long[] dis = new long[MAXN];
+    Edge[] head = new Edge[MAXN];
+    Edge[] rehead = new Edge[MAXN];
     long[] slack = new long[MAXN];
-    int[] pre = new int[MAXN];
-    void km(){
-        Arrays.fill(match, -1);
-        Arrays.fill(labley, 0);
-        for (int i = 0; i < N; i++) {
-            lablex[i] = -INF;
-            for (int j = 0; j < N; j++) {
-                lablex[i] = max(lablex[i], map[i][j]);
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            bfs(i);
-        }
-        ans  = 0;
-        for (int i = 0; i < N; i++) {
-            if (match[i] >= 0){
-                ans += map[match[i]][i];
-            }
-        }
+    private void zkw() {
+        ans = 0;
+        Arrays.fill(dis, 0);
+        do{
+            Arrays.fill(slack,INF);
+            do{
+                Arrays.fill(used, false);
+            } while (aug(source, INF) > 0);
+        } while (update());
     }
 
-
-    private void bfs(int x){
-        Arrays.fill(slack, INF);
-        Arrays.fill(visy, false);
-
-        long d;
-        int y = N; // 临时使用
-        match[y] = x;
-        int ny = N;
-        while (x!=-1){
-            // 找x的匹配
-            d = INF;
-            visy[y] = true;
-            for (int i = 0; i < N; i++) {
-                if (!visy[i] && map[x][i] != INF){
-                    if (lablex[x] + labley[i] - map[x][i] < slack[i]) {
-                        slack[i] = lablex[x] + labley[i] - map[x][i];
-                        pre[i] = y;
-                    }
-                    if (slack[i] < d) {
-                        d = slack[i];
-                        ny = i;
-                    }
-                }
+    private boolean update(){
+        long d = INF;
+        for (int i = 0; i < 2*N+2; i++) {
+            if (!used[i]) {
+                d = min(d, slack[i]);
+                slack[i] = INF;
             }
-            for (int i = 0; i <= N; i++) {
-                if (visy[i]){
-                    lablex[match[i]] -= d;
-                    labley[i] += d;
+        }
+        if (d == INF) {
+            return false;
+        }
+        for (int i = 0; i < 2*N+2; i++) {
+            if (used[i]){
+                dis[i] += d;
+            }
+        }
+        return true;
+    }
+
+    // 在最短路上进行多路增广
+    private long aug(int v, long cap) {
+        if (v == sink){
+            return cap;
+        }
+        used[v] = true;
+        int f = 0;
+        for (Edge e = head[v]; e != null; e = e.next){
+            if (!used[e.to] && e.cap > 0 ){
+                long t =  dis[e.to] + e.dis - dis[v];
+                if (t == 0){
+                    used[e.to] = true;
+                    long d = aug(e.to, min(cap-f, e.cap));
+                    ans += d * e.dis;
+                    e.cap -= d;
+                    e.rEdge.cap += d;
+                    f += d;
+                    if (f == cap){
+                        break;
+                    }
                 } else {
-                    slack[i] -= d;
+                    slack[e.to] = min(slack[e.to], t);
                 }
+
             }
-            y = ny;
-            x = match[y];
         }
-        while (y<N){
-            match[y] = match[pre[y]];
-            y = pre[y];
+        return f;
+    }
+
+    class Edge{
+        int from, to;
+        long cap, dis;
+        Edge next, rEdge;
+
+        public Edge(int from, int to, long cap, long dis, Edge next, Edge rEdge) {
+            this.from = from;
+            this.to = to;
+            this.cap = cap;
+            this.dis = dis;
+            this.next = next;
+            this.rEdge = rEdge;
         }
     }
+
 }
